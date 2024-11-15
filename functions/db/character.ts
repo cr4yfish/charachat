3e5ddb3511bd8881c98a15069f2a5349052a0966
Manuel <58395553+cr4yfish@users.noreply.github.com>
2024-11-15 23:36:27 +0100

    Fixed getCharacter, added updateCharacter

--- functions/db/character.ts
+++ functions/db/character.ts
@@ -7,13 +7,27 @@ import { createClient } from "@/utils/supabase/supabase";
 import { Character } from "@/types/db";
 
 export const getCharacter = cache(async (characterId: string): Promise<Character> => {
-    const { data, error } = await createClient().from("characters").select("*").eq("id", characterId).single();
+    const { data, error } = await createClient()
+        .from("characters")
+        .select(`
+            *,
+            profiles!characters_owner_fkey (*)
+        `)
+        .eq("id", characterId)
+        .single();
 
     if (error) {
         throw error;
     }
 
-    return data;
+    const owner = data.profiles;
+
+    delete data.profiles;
+
+    return {
+        ...data,
+        owner: owner
+    }
 })
 
 export const getCharacters = cache(async (): Promise<Character[]> => {
@@ -33,3 +47,17 @@ export const getCharacters = cache(async (): Promise<Character[]> => {
         }
     });
 })
+
+export const updateCharacter = async (character: Character): Promise<void> => {
+    const { error } = await createClient()
+        .from("characters")
+        .update({
+            ...character,
+            owner: character.owner.user
+        })
+        .eq("id", character.id);
+
+    if (error) {
+        throw error;
+    }
+}
