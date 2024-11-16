38a49b2f1e702c44bbc0492a39f94ff5a75452e1
Manuel <58395553+cr4yfish@users.noreply.github.com>
2024-11-16 17:37:24 +0100

    added missing link

--- components/chat/ChatCardSmall.tsx
+++ components/chat/ChatCardSmall.tsx
@@ -9,10 +9,12 @@ import { getLatestChatMessage } from "@/functions/db/messages";
 import { Skeleton } from "../ui/skeleton";
 import { formatLastMessageTime, truncateText } from "@/lib/utils";
 import { Avatar } from "@nextui-org/avatar";
+import ConditionalLink from "../utils/ConditionalLink";
 
 type Props = {
     chat: Chat;
     setChats?: React.Dispatch<React.SetStateAction<Chat[]>>;
+    hasLink?: boolean;
 }
 
 export default function ChatCardSmall(props: Props) {
@@ -34,45 +36,45 @@ export default function ChatCardSmall(props: Props) {
 
     return (
         <>
-       
-        <Card>
-            <CardBody className="flex flex-row gap-2 items-center justify-start">
+        <ConditionalLink active={props.hasLink !== undefined} href={`/chat/${props.chat.id}`}>
+            <Card>
+                <CardBody className="flex flex-row gap-2 items-center justify-start">
 
-                <Avatar src={props.chat.character.image_link} size="md" />
+                    <Avatar src={props.chat.character.image_link} size="md" />
 
-                <div className="flex flex-col w-full">
-                    <div className="flex flex-row items-center justify-between w-full">
-                        <h3 className="font-bold text-lg">{props.chat.character.name}</h3>
-                        {props.chat.last_message_at && 
-                            <span className="text-xs dark:text-slate-400">{formatLastMessageTime(new Date(props.chat.last_message_at))}</span>
+                    <div className="flex flex-col w-full">
+                        <div className="flex flex-row items-center justify-between w-full">
+                            <h3 className="font-bold text-lg">{props.chat.character.name}</h3>
+                            {props.chat.last_message_at && 
+                                <span className="text-xs dark:text-slate-400">{formatLastMessageTime(new Date(props.chat.last_message_at))}</span>
+                            }
+                        </div>
+                        
+                        {isLoadingLatestMessage && 
+                            <motion.div
+                                initial={{ opacity: 0 }}
+                                animate={{ opacity: 1 }}
+                                transition={{ delay: 0.3 }}
+                            >
+                                <Skeleton className="w-full h-5" />
+                            </motion.div>
+                        }
+                        {!isLoadingLatestMessage && latestMessage?.content && 
+                            <motion.p 
+                                initial={{ opacity: 0 }}
+                                animate={{ opacity: 1 }}
+                                transition={{ delay: 0.3 }}
+                                className="dark:text-slate-400 single-line text-sm"
+                            >
+                                {truncateText(latestMessage.content, 40)}
+                            </motion.p>
                         }
                     </div>
-                    
-                    {isLoadingLatestMessage && 
-                        <motion.div
-                            initial={{ opacity: 0 }}
-                            animate={{ opacity: 1 }}
-                            transition={{ delay: 0.3 }}
-                        >
-                            <Skeleton className="w-full h-5" />
-                        </motion.div>
-                    }
-                    {!isLoadingLatestMessage && latestMessage?.content && 
-                        <motion.p 
-                            initial={{ opacity: 0 }}
-                            animate={{ opacity: 1 }}
-                            transition={{ delay: 0.3 }}
-                            className="dark:text-slate-400 single-line text-sm"
-                        >
-                            {truncateText(latestMessage.content, 40)}
-                        </motion.p>
-                    }
-                </div>
 
-            </CardBody>
+                </CardBody>
 
-        </Card>
-
+            </Card>
+        </ConditionalLink>
         </>
     )
 }
