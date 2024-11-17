185ec89020031da98be861a6bff5c703cbf754f8
Manuel <58395553+cr4yfish@users.noreply.github.com>
2024-11-17 10:42:43 +0100

    Fixed lots of design issues with story page

--- app/c/[characterId]/story/[storyId]/page.tsx
+++ app/c/[characterId]/story/[storyId]/page.tsx
@@ -1,9 +1,6 @@
 "use server";
 
-import { Spacer } from "@nextui-org/spacer"
-
 import CharacterCard from "@/components/character/CharacterCard";
-import BackLink from "@/components/utils/BackLink";
 import { getStory } from "@/functions/db/stories";
 import Image from "next/image";
 import { Button } from "@/components/utils/Button";
@@ -19,43 +16,36 @@ export default async function Story({ params: { storyId, characterId } }: { para
 
     return (
         <>
-         <div className="z-20 absolute bottom-0 left-0 w-full flex gap-1 items-center justify-center pb-5 px-4">
-            <StartStoryButton story={story} profile={profile} />
-            {profile.user == story.creator.user && 
-                <Link href={`/c/${characterId}/story/${storyId}/edit`}><Button fullWidth size="lg" color="warning" variant="shadow">Edit Story</Button></Link> 
-            }
-        </div>
+        <div className="flex flex-col gap-2 relative px-4 py-6 pb-32">
 
-        <div className="flex flex-col gap-2 relative px-4 py-6">
-
-            <div className="flex items-center gap-2">
-                <BackLink />
-            </div>
-
-            <div className="absolute -z-10 top-0 left-0 w-full h-[66vh]">
+            <div className="absolute -z-10 left-0 -top-12 w-full h-[66vh]">
                 <Image src={story.image_link} layout="fill" objectFit="cover" alt={story.title} />
             </div>
-
-   
-            <Spacer y={56} />
-
-            <h1 className="font-bold text-2xl">{story.title}</h1>
             
-            <CharacterCard fullWidth character={story.character} hasLink />
+            <div className="flex flex-col gap-2 pt-[45vh]">
 
-            <Spacer />
+                <h1 className="font-bold text-2xl">{story.title}</h1>
+                
+                <CharacterCard fullWidth character={story.character} hasLink />
 
+                <div className="relative w-full flex gap-2 items-center justify-center py-4">
+                    <StartStoryButton story={story} profile={profile} />
+                    {profile.user == story.creator.user && 
+                        <Link href={`/c/${characterId}/story/${storyId}/edit`}><Button fullWidth size="lg" color="warning" variant="flat">Edit</Button></Link> 
+                    }
+                </div>
 
-            <div className="prose dark:prose-invert">
-                <h2 className="text-2xl font-bold">Story Details</h2>
-                <h3>Description</h3>
-                <p>{story.description}</p>
+                <div className="prose dark:prose-invert">
+                    <h2 className="text-2xl font-bold">Story Details</h2>
+                    <h3>Description</h3>
+                    <p>{story.description}</p>
 
-                <h3>Story</h3>
-                <p>{story.story}</p>
+                    <h3>Story</h3>
+                    <p>{story.story}</p>
 
-                <h3>First Message</h3>
-                <p>{story.first_message}</p>
+                    <h3>First Message</h3>
+                    <p>{story.first_message}</p>
+                </div>
             </div>
 
         </div>
