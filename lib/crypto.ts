381b6bc1fa2008c2959c8896a329f952e31b4b86
Manuel <58395553+cr4yfish@users.noreply.github.com>
2024-11-17 10:04:43 +0100

    Improved encryption functions to be safer

--- lib/crypto.ts
+++ lib/crypto.ts
@@ -4,16 +4,38 @@ export const generateKey = (password: string, salt: string): Buffer => {
     return crypto.pbkdf2Sync(password, salt, 100000, 32, 'sha256');
 };
 
+/**
+ * Encrypts a message using the provided key
+ * @param message 
+ * @param key 
+ * @returns string
+ * @throws Error if the message is already encrypted
+ */
 export const encryptMessage = (message: string, key: Buffer): string => {
+    if(message.startsWith("ENC:")) {
+        throw new Error("Trying to encrypt a message that is already encrypted");
+    }
+
     const iv = crypto.randomBytes(16);
     const cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
     let encrypted = cipher.update(message, 'utf8', 'hex');
     encrypted += cipher.final('hex');
-    return iv.toString('hex') + ':' + encrypted;
+    return "ENC:" + iv.toString('hex') + ':' + encrypted;
 };
 
+/**
+ * Decrypts a message using the provided key
+ * @param encryptedMessage 
+ * @param key 
+ * @returns string
+ * @throws Error if the message is not encrypted
+ */
 export const decryptMessage = (encryptedMessage: string, key: Buffer): string => {
-    const [ivHex, encrypted] = encryptedMessage.split(':');
+    if(!encryptedMessage.startsWith("ENC:")) {
+        throw new Error("Trying to decrypt a message that is not encrypted");
+    }
+    const msgWithoutPrefix = encryptedMessage.slice(4);
+    const [ivHex, encrypted] = msgWithoutPrefix.split(':');
     const iv = Buffer.from(ivHex, 'hex');
     const decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
     let decrypted = decipher.update(encrypted, 'hex', 'utf8');
