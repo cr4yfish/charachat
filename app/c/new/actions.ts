5a9016aac1b1ff060b1f2fb62a5a74e6b8340785
Manuel <58395553+cr4yfish@users.noreply.github.com>
2024-11-18 12:13:17 +0100

    Adjusted server side code for c/new for new client side render

--- app/c/new/actions.ts
+++ app/c/new/actions.ts
@@ -1,11 +1,11 @@
 "use server";
 
-import { redirect } from "next/navigation";
 import { v4 as uuidV4 } from "uuid";
 
 import { createClient } from "@/utils/supabase/supabase";
+import { Character } from "@/types/db";
 
-export const saveCharacter = async (formData: FormData): Promise<void> => {
+export const saveCharacter = async (char: Character): Promise<{ data: Character, error: string }> => {
 
     const { data: { user } } = await createClient().auth.getUser();
 
@@ -13,30 +13,21 @@ export const saveCharacter = async (formData: FormData): Promise<void> => {
         throw new Error("Only authenticated users can create characters");
     }
 
-    const character = {
-        name: formData.get("name") as string,
-        description: formData.get("description") as string,
-        bio: formData.get("bio") as string,
-        image_link: formData.get("image_link") as string,
-        intro: formData.get("intro") as string,
-        book: formData.get("book") as string,
+    const newChar = {
+        ...char,
         id: uuidV4(),
-        owner: user.id,
-        llm: "gpt-4o-mini"
+        owner: user.id
     }
 
     const { data, error } = await createClient()
         .from("characters")
-        .insert([character])
-        .eq("id", character.id)
+        .insert(newChar)
+        .eq("id", newChar.id)
         .select("*").single();
 
-    if (error) {
-        throw error;
-    }
-
-    if(data) {
-        redirect(`/c/${data.id}`);
+    return {
+        data, 
+        error: error?.message || ""
     }
 
 }
--- app/c/new/page.tsx
+++ app/c/new/page.tsx
@@ -1,10 +1,6 @@
 "use server";
 
-import { Switch } from "@nextui-org/switch";
-import { Input } from "@nextui-org/input";
-import { saveCharacter } from "./actions";
-import TextareaWithCounter from "@/components/utils/TextareaWithCounter";
-import { ServerActionButton } from "@/components/utils/ServerActionButton";
+import CharacterNewMain from "@/components/character/CharacterNewMain";
 
 export default async function NewCharacter() {
 
@@ -14,65 +10,7 @@ export default async function NewCharacter() {
             <h2 className="font-bold text-2xl">Create a new Character</h2>
         </div>
         
-        <form className="flex flex-col gap-4">
-            <TextareaWithCounter 
-                name="name" 
-                isRequired
-                label="Character Name"
-                placeholder="Albert Einstein"
-                description="Name of the Character" 
-                maxLength={28}
-                minRows={1}
-                maxRows={1} 
-            />
-            <TextareaWithCounter 
-                name="description" 
-                isRequired
-                label="Character Description"
-                placeholder="Physicist, mathematician, and author"
-                description="Very short description of the Character" 
-                maxLength={50} 
-            />
-            <TextareaWithCounter 
-                name="bio" 
-                isRequired
-                label="Character Bio"
-                placeholder="Albert Einstein was a German-born theoretical physicist who developed the theory of relativity, one of the two pillars of modern physics. He was born in 1879 and died in 1955."
-                description="Facts about the character. Who are they? What do they do? Where do they come from?" 
-                maxLength={280} 
-            />
-            <Input 
-                name="image_link" 
-                isRequired 
-                label="Image Link" 
-                placeholder="https://i.imgur.com/XgbZdeAb.jpg" 
-                description="Direct link to an image" 
-            />
-            <TextareaWithCounter 
-                name="intro" 
-                label="Character Intro"
-                isRequired
-                placeholder="Hello, I'm Albert Einstein. I'm a physicist, mathematician, and author. I developed the theory of relativity, one of the two pillars of modern physics."
-                description="Introduction of the character. Describe how the character would introduce themselves." 
-                maxLength={280} 
-            />
-            <TextareaWithCounter 
-                name="book" 
-                label="Character Book"
-                description="All background information you can provide - the more the better. Background stories, relationsships, example dialogs etc." 
-                maxLength={1000} 
-            />
-            <Switch isDisabled>Private</Switch>
-            <ServerActionButton 
-                formAction={saveCharacter} 
-                type="submit" 
-                color="primary" 
-                variant="shadow" 
-                size="lg" 
-            >
-                Create
-            </ServerActionButton>
-        </form>
+        <CharacterNewMain />
         </>
     )
 }
