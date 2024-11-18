02019327aae10c267c0f323169769351e4871a0f
Manuel <58395553+cr4yfish@users.noreply.github.com>
2024-11-18 08:25:09 +0100

    Improved error messages on chat page

--- app/chat/[chatId]/page.tsx
+++ app/chat/[chatId]/page.tsx
@@ -11,6 +11,9 @@ import { getMessages } from "@/functions/db/messages";
 
 import { Chat as ChatType, Message, Profile } from "@/types/db";
 import { redirect } from "next/navigation";
+import LoginButton from "@/components/auth/LoginButton";
+import Link from "next/link";
+import { Button } from "@/components/utils/Button";
 
 
 export default async function Chat({ params: { chatId } } : { params: { chatId: string } }) {
@@ -25,7 +28,31 @@ export default async function Chat({ params: { chatId } } : { params: { chatId: 
     }
 
     if (!chat) {
-        return <p>Chat not found</p>
+        return (
+        <div className="px-4 py-6 flex flex-col items-center justify-between h-full">
+            <div className="flex flex-col prose dark:prose-invert prose-h2:mt-3 prose-h2:mb-2">
+                <h1>Error: Didnt find a Chat with this id!</h1>
+                <div className="flex flex-col m-0 mb-2">
+                    <span className="m-0">The chat id</span>
+                    <pre className="m-0 select-all">{chatId}</pre>
+                </div>
+                
+                <h2>This could have a few causes:</h2>
+                <ul>
+                    <li>The chat was deleted</li>
+                    <li>The chat belongs to another account</li>
+                    <li>You are not logged in</li>
+                </ul>
+                <h2>To fix this</h2>
+                <ul>
+                    <li>Try logging out and then back in</li>
+                    <li>Check if you are logged in with the correct account</li>
+                    <li>Report this issue</li>
+                </ul>
+            </div>
+            <Link href={"/"} className="w-full"><Button fullWidth size="lg" color="primary">Home</Button></Link>
+        </div>
+        )
     }
 
     let initMessages: Message[] = [];
@@ -35,7 +62,18 @@ export default async function Chat({ params: { chatId } } : { params: { chatId: 
 
     if (!key) {
         console.error("No key cookie");
-        return <>No key!</>
+        return (
+            <>
+            <div className="px-4 py-6 flex flex-col items-center justify-center">
+                <div className="flex flex-col prose dark:prose-invert">
+                    <h1>Error: Didnt find an Encryption Key in your session data!</h1>
+                    <p>Please report this issue. To fix this, please log out and then back in.</p>
+                </div>
+                <LoginButton showLogout isLoggedIn={true} />
+            </div>
+            
+            </>
+        )
     }
 
     try {
