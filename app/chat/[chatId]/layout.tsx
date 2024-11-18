382e607f14d7861b19e98ced1f23e1373f3e5315
Manuel <58395553+cr4yfish@users.noreply.github.com>
2024-11-18 21:42:21 +0100

    Fixed chat overflow

--- app/chat/[chatId]/layout.tsx
+++ app/chat/[chatId]/layout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Layout({children} : {children: React.ReactNode | React.ReactNode[]}) {
     return (
-        <div className="flex flex-col justify-between overflow-hidden overflow-y-hidden h-svh w-screen">
+        <div className="flex flex-col justify-between overflow-hidden overflow-y-hidden h-svh w-full">
             {children}
         </div>
     )
