5a17b72491a8ace89e66c244d5d81dae7fcf29b5
Manuel <58395553+cr4yfish@users.noreply.github.com>
2024-11-20 17:26:46 +0100

    Added hideLogoTextOnMobile option

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,10 +1,14 @@
 import { BotIcon } from "lucide-react"
 
-export default function Logo() {
+type Props = {
+    hideTextOnMobile?: boolean;
+}
+
+export default function Logo(props: Props) {
     return (
         <div className="flex flex-row items-center gap-2 w-fit">
             <BotIcon size={24} />
-            <span className="text-lg font-bold">charachat</span>
+            <span className={`text-lg font-bold ${props.hideTextOnMobile && "max-sm:hidden"}`}>charachat</span>
         </div>
         
     )
