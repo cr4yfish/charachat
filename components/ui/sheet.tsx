a16478d806f54403daecb4ca6dc5370414e7dfb3
Manuel <58395553+cr4yfish@users.noreply.github.com>
2024-11-20 17:06:08 +0100

    Improved sheet design

--- components/ui/sheet.tsx
+++ components/ui/sheet.tsx
@@ -31,7 +31,7 @@ const SheetOverlay = React.forwardRef<
 SheetOverlay.displayName = SheetPrimitive.Overlay.displayName
 
 const sheetVariants = cva(
-  "fixed z-50 gap-4 bg-white p-6 shadow-lg transition ease-in-out data-[state=closed]:duration-300 data-[state=open]:duration-500 data-[state=open]:animate-in data-[state=closed]:animate-out dark:bg-neutral-950",
+  "fixed z-50 gap-4 bg-white p-6 shadow-lg transition ease-in-out data-[state=closed]:duration-300 data-[state=open]:duration-500 data-[state=open]:animate-in data-[state=closed]:animate-out dark:bg-zinc-900/60 backdrop-blur-lg border-l dark:border-zinc-700 ",
   {
     variants: {
       side: {
