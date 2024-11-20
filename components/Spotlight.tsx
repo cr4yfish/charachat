0f35938c667b8e48357967cb129bb2365db14ef4
Manuel <58395553+cr4yfish@users.noreply.github.com>
2024-11-20 16:30:17 +0100

    added image blur to spotlight

--- components/Spotlight.tsx
+++ components/Spotlight.tsx
@@ -14,6 +14,7 @@ export default function Spotlight(props: Props) {
 
     return (
         <>
+        <div className="w-full h-fit relative overflow-visible">
         <Card 
             className={`
                 dark:bg-zinc-600/10 relative overflow-hidden rounded-3xl
@@ -26,12 +27,20 @@ export default function Spotlight(props: Props) {
             <CardContent>
                 <CharacterCard data={props.character} hasLink noBg />
 
-                <Image className=" object-cover -z-10" src={props.character.image_link ?? ""} layout="fill" alt="" />
+                <Image className=" object-cover -z-10 opacity-25" src={props.character.image_link ?? ""} layout="fill" alt="" />
             </CardContent>
             <CardFooter>
                 
             </CardFooter>
         </Card>
+
+        <div className="absolute -z-20 scale-[150%] scale-x-[200%] top-0 left-0 w-full h-full blur-xl ">
+            <Image className=" object-cover z-50" src={props.character.image_link ?? ""} layout="fill" alt="" />
+        </div>
+        
+        </div>
+   
+        
         </>
     )
 }
