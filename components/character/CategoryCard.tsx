303b5c407fdd53d81103e13e23dcce2a1de4c269
Manuel <58395553+cr4yfish@users.noreply.github.com>
2024-11-20 01:47:15 +0100

    Trying to fix offset text in category card on safari

--- components/character/CategoryCard.tsx
+++ components/character/CategoryCard.tsx
@@ -19,12 +19,12 @@ export default function CategoryCard(props: Props) {
             isPressable={props.isButton}
             onClick={props.onClick}
             className={`
-                w-fit max-w-fit px-4 py-2 rounded-full dark:bg-transparent
+                w-full max-w-fit flex items-center justify-center px-4 py-2 rounded-full dark:bg-transparent
                 backdrop-blur-xl border-1 dark:border-zinc-600 shadow-none text-sm
                 ${props.isSelected && "dark:bg-zinc-700"}
             `}
             >
-            <CardBody className="p-0 w-fit min-w-max h-full flex items-center justify-center">
+            <CardBody className="p-0 w-full min-w-max h-full flex flex-row items-center justify-center">
                 {props.category.title}
             </CardBody>
         </Card>
