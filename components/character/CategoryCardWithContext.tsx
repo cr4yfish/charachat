9c334a3c8152b01ada7cec2ea8c4c84e233167ad
Manuel <58395553+cr4yfish@users.noreply.github.com>
2024-11-22 15:12:05 +0100

    Fixed hover state when active category card

--- components/character/CategoryCardWithContext.tsx
+++ components/character/CategoryCardWithContext.tsx
@@ -23,8 +23,7 @@ export default function CategoryCardWithContext(props: Props) {
             className={`
                 w-full max-w-fit flex items-center justify-center px-4 py-3 rounded-full dark:bg-zinc-600/40
                 backdrop-blur-xl border-1 dark:border-none shadow-none text-sm font-medium
-                dark:hover:bg-zinc-700/40
-                ${(props.data.id == currentCategory?.id) && "dark:bg-white dark:text-zinc-950"}
+                ${(props.data.id == currentCategory?.id) ? "dark:bg-white dark:text-zinc-950" : "dark:hover:bg-zinc-700/40"}
             `}
             >
             <CardBody className="p-0 w-full min-w-max h-full flex flex-row items-center justify-center">
