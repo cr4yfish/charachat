e0f81f91cff803ec37f5c3994283176ac75f5acf
Manuel <58395553+cr4yfish@users.noreply.github.com>
2024-11-22 15:18:04 +0100

    Removed logoutButton, removed github link, changed scrollable content

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -16,7 +16,6 @@ import { getCurrentUser } from "@/functions/db/auth";
 import { Chat, Profile } from "@/types/db";
 
 import SidebarLink from "./SidebarLink";
-import LoginButton from "./auth/LoginButton";
 import Logo from "./Logo";
 import Link from "next/link";
 import InfiniteListLoader from "./InfiniteListLoader";
@@ -40,23 +39,24 @@ export async function LeftSidebar() {
 
   return (
     <Sidebar>
-      <SidebarHeader />
-      <SidebarContent>
-
-        <SidebarGroup>
-          <Link href={"/"}><Logo /></Link>
-        </SidebarGroup>
+      <SidebarHeader>
         
-        <SidebarGroup>
-          <SidebarGroupContent className="flex flex-col gap-2">
-            <SidebarLink link="/" isLoggedIn={isLoggedIn} icon="explore" label="Explore" enableAnon />
-            <SidebarLink link={`/user/${profile?.user}/chats`} isLoggedIn={isLoggedIn} icon="chat" label="Your Chats" />
-            <SidebarLink link={`/user/${profile?.user}/characters`} isLoggedIn={isLoggedIn} icon="people" label="Your Characters" />
-            <SidebarLink link={`/user/${profile?.user}/stories`} isLoggedIn={isLoggedIn} icon="book" label="Your Stories" />
-            <SidebarLink link={`https://github.com/cr4yfish/charachat`} isLoggedIn={isLoggedIn} icon="open_in_new" label="GitHub" isExternal />
-          </SidebarGroupContent>
-        </SidebarGroup>
+          <SidebarGroup>
+            <Link href={"/"}><Logo /></Link>
+          </SidebarGroup>
+          
+          <SidebarGroup>
+            <SidebarGroupContent className="flex flex-col gap-2">
+              <SidebarLink link="/" isLoggedIn={isLoggedIn} icon="explore" label="Explore" enableAnon />
+              <SidebarLink link={`/user/${profile?.user}/chats`} isLoggedIn={isLoggedIn} icon="chat" label="Your Chats" />
+              <SidebarLink link={`/user/${profile?.user}/characters`} isLoggedIn={isLoggedIn} icon="people" label="Your Characters" />
+              <SidebarLink link={`/user/${profile?.user}/stories`} isLoggedIn={isLoggedIn} icon="book" label="Your Stories" />
+            </SidebarGroupContent>
+          </SidebarGroup>
+        
+      </SidebarHeader>
 
+      <SidebarContent>
 
         {profile !== undefined && 
         <SidebarGroup>
@@ -74,13 +74,11 @@ export async function LeftSidebar() {
         }
 
       </SidebarContent>
+
       <SidebarFooter className="w-full flex flex-col gap-2 items-start px-4 py-6">
-        <LoginButton 
-            isLoggedIn={profile !== undefined} 
-            showLogout 
-        />
         {profile !== undefined && <ProfileCard profile={profile} />}
       </SidebarFooter>
+
     </Sidebar>
     )
   }
