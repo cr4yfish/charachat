934ea6dfa6dc8082caaea30732c166ac5a42f28c
Manuel <58395553+cr4yfish@users.noreply.github.com>
2024-11-23 18:22:36 +0100

    moved importers to own components, added wikipedia importer

--- components/character/CharacterNewImport.tsx
+++ components/character/CharacterNewImport.tsx
@@ -1,71 +1,10 @@
 "use client";
 
 import { Character, Profile } from "@/types/db";
-import { v4 as uuidv4 } from "uuid";
 import {Tabs, Tab} from "@nextui-org/tabs";
-import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "../ui/card";
-import { Card as NextUICard, CardBody as NextUICardBody } from "@nextui-org/card";
-import { Input } from "@nextui-org/input";
-import Icon from "../utils/Icon";
-import { useEffect, useRef, useState } from "react";
-import { Spinner } from "@nextui-org/spinner";
-import Markdown from "react-markdown";
-import { Avatar } from "@nextui-org/avatar";
-import { truncateText } from "@/lib/utils";
+import AnimeImport from "./AnimeImport";
+import WikipediaImport from "./WikipediaImport";
 
-const query = `
-    query ($search: String!) {
-        Page {
-            characters(search: $search) {
-                id,
-                name {
-                    full
-                },
-                image {
-                    medium
-                },
-                description,
-                media {
-                    nodes {
-                        title {
-                            romaji
-                        },
-                        description,
-                        type
-                    }
-                }
-            }
-        }
-    }
-`
-
-const url = "https://graphql.anilist.co";
-
-type AnimeChar = {
-    id: number;
-    name: {
-        full: string;
-    };
-    image: {
-        medium: string;
-    };
-    description: string;
-    media: {
-        nodes: Array<{
-            title: {
-                romaji: string;
-            },
-            type: string;
-            description: string;
-        }>
-    },
-    gender: string,
-    dateOfBirth: {
-        year: number;
-        month: number;
-        day: number;
-    }
-}
 
 type Props = {
     character: Character;
@@ -73,106 +12,9 @@ type Props = {
     profile: Profile;
 }
 
-type AnimeCharCardProps = {
-    character: AnimeChar;
-    callback?: (char: AnimeChar) => void;
-}
-
-function AnimeCharCard(props: AnimeCharCardProps) {
-    const handleCallback = () => {
-        if(props.callback) {
-            props.callback(props.character);
-        }
-    }
-
-    return (
-        <>
-        <NextUICard isPressable onClick={handleCallback}>
-            <NextUICardBody className="flex flex-row gap-2">
-                <div>
-                    <Avatar src={props.character.image.medium} size="lg" alt={props.character.name.full} />
-                </div>
-                <div className="flex flex-col">
-                    <div className="flex flex-col">
-                        <span className="text-xs dark:text-zinc-400">{props.character.media.nodes[0].title.romaji}</span>
-                        <span className="font-bold">{props.character.name.full}</span>
-                    </div>
-                   
-                    <Markdown className="text-xs dark:text-zinc-400">{truncateText(props.character.description, 280)}</Markdown>
-                </div>
-            </NextUICardBody>
-        </NextUICard>
-        </>
-    )
-}
 
 export default function CharacterNewImport(props: Props) {
-    const [search, setSearch] = useState<string>("");
-    const [animeCharacters, setAnimeCharacters] = useState<AnimeChar[]>([]);
-    const searchTimeout = useRef<NodeJS.Timeout>();
-    const [isLoading, setIsLoading] = useState<boolean>(false);
 
-    const handleImportCharacter = (char: AnimeChar) => {
-        // convert to Character and update upstream
-
-        const character: Character = {
-            id: uuidv4(),
-            name: char.name.full,
-            description: char.description,
-            image_link: char.image.medium,
-            owner: props.profile,
-            book: `${char.media.nodes[0].type}: ${char.media.nodes[0].title.romaji} Description: ${char.media.nodes[0].description}`,
-        } as Character;
-
-        props.setCharacter(character);
-
-    }
-
-    const handleSearch = async (searchString: string) => {
-        setIsLoading(true);
-        const res = await fetch(url, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "application/json"
-            },
-            body: JSON.stringify({
-                query: query,
-                variables: {
-                    search: searchString
-                }
-            })
-        })
-
-        if(res.status == 200) {
-            const json = await res.json();
-            setAnimeCharacters(json.data.Page.characters);
-        } else {
-            console.error("Error fetching data");
-        }
-        setIsLoading(false);
-    }
-
-    useEffect(() => {
-        if(searchTimeout.current) {
-            clearTimeout(searchTimeout.current);
-        }
-
-        searchTimeout.current = setTimeout(() => {
-            if(search.length == 0) {
-                // reset results
-                setAnimeCharacters([]);
-            } else {
-                handleSearch(search);
-            }
-        }, 500);
-
-        return () => {
-            if(searchTimeout.current) {
-                clearTimeout(searchTimeout.current);
-            }
-        }
-    }, [search])
 
     return (
         <>
@@ -180,46 +22,17 @@ export default function CharacterNewImport(props: Props) {
             <h1 className="text-xl font-bold">Import</h1>
             
             <Tabs className="w-full">
-                <Tab title="Anime" className="w-full">
-                    <Card className="w-full">
-                        <CardHeader>
-                            <CardDescription>Import from Anime</CardDescription>
-                            <CardTitle>Search Character</CardTitle>
-                        </CardHeader>
-                        <CardContent className="flex flex-col gap-4">
-
-                            <Input 
-                                radius="full" size="lg"
-                                startContent={isLoading ? <Spinner size="sm" color="default" /> : <Icon filled>search</Icon>} 
-                                placeholder="Search Character name" 
-                                value={search} onValueChange={setSearch}
-                            />          
-
-                            <div className="flex flex-col gap-2">
-                                <h3 className="font-bold text-sm dark:text-zinc-400">Search Results</h3>
-                                {animeCharacters.slice(0,10).map((char, index) => (
-                                    <AnimeCharCard 
-                                        character={char} 
-                                        callback={handleImportCharacter}
-                                        key={index} 
-                                    /> 
-                                ))}
-                            </div>
-                        
-                        </CardContent>
-                        <CardFooter>
-
-                        </CardFooter>
-                    </Card>
+                <Tab title="Anime & Manga" className="w-full">
+                    <AnimeImport 
+                        setCharacter={props.setCharacter}
+                        profile={props.profile}
+                    />  
                 </Tab>
-                <Tab title="Book">
-                    <span>Not implemented yet.</span>
-                </Tab>
-                <Tab title="C.ai">
-                    <span>Not implemented yet.</span>
-                </Tab>
-                <Tab title="Silly Tavern">
-                    <span>Not implemented yet.</span>
+                <Tab title="Wikipedia">
+                    <WikipediaImport 
+                        setCharacter={props.setCharacter}
+                        profile={props.profile}
+                    />
                 </Tab>
             </Tabs>
         </div>
---
+++ components/character/WikipediaImport.tsx
@@ -0,0 +1,205 @@
+"use client";
+
+import { useState, useEffect, useRef } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "../ui/card";
+import { Card as NextUICard, CardBody as NextUICardBody } from "@nextui-org/card";
+import { Input } from "@nextui-org/input";
+import Icon from "../utils/Icon";
+import { Spinner } from "@nextui-org/spinner";
+import Markdown from "react-markdown";
+import { Avatar } from "@nextui-org/avatar";
+import { truncateText } from "@/lib/utils";
+import { Character, Profile } from "@/types/db";
+
+
+const url = 'https://en.wikipedia.org/w/rest.php/v1/search/page?q=';
+
+type WikiPage = {
+    id: number;
+    title: string;
+    description: string;
+    expert: string;
+    thumbnail?: {
+        url: string;
+    }
+}
+
+type WikiPageCardProps = {
+    wikipage: WikiPage;
+    callback?: (char: WikiPage) => void;
+    isLoading?: boolean;
+}
+
+function WikiPageCard(props: WikiPageCardProps) {
+    const handleCallback = () => {
+        if(props.callback) {
+            props.callback(props.wikipage);
+        }
+    }
+
+    return (
+        <>
+        <NextUICard isDisabled={props.isLoading} isPressable onClick={handleCallback}>
+            <NextUICardBody className="flex flex-row gap-2">
+                <div>
+                    <Avatar src={props.wikipage.thumbnail ? props.wikipage.thumbnail.url : ""} size="lg" alt={props.wikipage.title} />
+                </div>
+                <div className="flex flex-col">
+                    <div className="flex flex-col">
+                        <span className="font-bold">{props.wikipage.title}</span>
+                    </div>
+                   
+                    <Markdown className="text-xs dark:text-zinc-400">{truncateText(props.wikipage.description, 280)}</Markdown>
+                </div>
+            </NextUICardBody>
+        </NextUICard>
+        </>
+    )
+}
+
+type Props = {
+    setCharacter: (character: Character) => void;
+    profile: Profile;
+}
+
+export default function WikipediaImport(props: Props) {
+    const [search, setSearch] = useState<string>("");
+    const [wikiPages, setWikiPages] = useState<WikiPage[]>([]);
+    const searchTimeout = useRef<NodeJS.Timeout>();
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [isImporting, setIsImporting] = useState<boolean>(false);
+
+
+    const handleImportCharacter = async (page: WikiPage) => {
+        setIsImporting(true);
+
+        // get full page
+        const res = await fetch("https://en.wikipedia.org/w/rest.php/v1/page/" + page.title, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+            },
+            
+        })
+
+        let book = "";
+        
+        if(res.status == 200) {
+            const json = await res.json();
+            book = json.source;
+        } else {
+            console.error("Error fetching data");
+        }
+
+        // regex to match {{  }} with whitespace, text and nested {{ }} inside
+        book = book.replace(/{{([^{}]*({{[^{}]*}}[^{}]*)*)}}/g, "");
+
+        // regex remove all lines with | at start
+        book = book.replace(/^\|.*$/gm, "");
+
+        // remove {{ }} until no match
+        while(/{{[^{}]*}}/.test(book)) {
+            book = book.replace(/{{[^{}]*}}/g, "");
+        }
+
+        // regex remove all lines with no text
+        book = book.replace(/^\s*$/gm, "");
+
+        book = book.slice(0,1000);
+
+        console.log("Rest:",book);
+
+        
+        // convert to Character and update upstream
+        const character: Character = {
+            id: uuidv4(),
+            name: page.title,
+            description: page.description,
+            image_link: page.thumbnail ? page.thumbnail.url : "",
+            owner: props.profile,
+            book: book
+         } as Character;
+
+        props.setCharacter(character);
+
+    }
+
+    const handleSearch = async (searchString: string) => {
+        setIsLoading(true);
+        const res = await fetch(url + searchString, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+            },
+            
+        })
+
+        if(res.status == 200) {
+            const json = await res.json();
+            setWikiPages(json.pages);
+        } else {
+            console.error("Error fetching data");
+        }
+        setIsLoading(false);
+    }
+
+    useEffect(() => {
+        if(searchTimeout.current) {
+            clearTimeout(searchTimeout.current);
+        }
+
+        searchTimeout.current = setTimeout(() => {
+            if(search.length == 0) {
+                // reset results
+                setWikiPages([]);
+            } else {
+                handleSearch(search);
+            }
+        }, 500);
+
+        return () => {
+            if(searchTimeout.current) {
+                clearTimeout(searchTimeout.current);
+            }
+        }
+    }, [search])
+
+    return (
+        <>
+        <Card className="w-full">
+            <CardHeader>
+                <CardDescription>Import from Wikipedia</CardDescription>
+                <CardTitle>Search Wikipedia</CardTitle>
+            </CardHeader>
+            <CardContent className="flex flex-col gap-4">
+
+                <Input 
+                    radius="full" size="lg"
+                    startContent={isLoading ? <Spinner size="sm" color="default" /> : <Icon filled>search</Icon>} 
+                    placeholder="Search Wikipedia Article" 
+                    value={search} onValueChange={setSearch}
+                />          
+
+                <div className="flex flex-col gap-2">
+                    <h3 className="font-bold text-sm dark:text-zinc-400">Search Results</h3>
+                    {wikiPages.slice(0,10).map((page, index) => (
+                        <WikiPageCard 
+                            wikipage={page} 
+                            callback={handleImportCharacter}
+                            key={index} 
+                            isLoading={isImporting}
+                        /> 
+                    ))}
+                </div>
+            
+            </CardContent>
+            <CardFooter>
+
+            </CardFooter>
+        </Card>
+        </>
+    )
+}
