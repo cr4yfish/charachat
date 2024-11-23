db199239f369d42770aa319123ba71fad66e6043
Manuel <58395553+cr4yfish@users.noreply.github.com>
2024-11-23 15:29:20 +0100

    Removed duplicate info from auth page

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,9 +1,6 @@
 "use server"
 
-import Link from "next/link"
-
 import LoginCard from "@/components/auth/LoginCard"
-import { Button } from "@/components/utils/Button"
 import { checkIsLoggedIn } from "@/functions/db/auth"
 import { redirect } from "next/navigation"
 
@@ -18,11 +15,7 @@ export default async function Login() {
     return (
         <>
         <div className="z-10 px-4 py-6 h-screen flex flex-col gap-4 items-center justify-center pt-[15vh] pb-[33vh]">
-            <p>Login or sign up</p>
             <LoginCard />
-            <Link href="/signup">
-                <Button isDisabled variant="flat" color="secondary" size="lg">Sign up instead</Button>
-            </Link>
         </div>
         </>
     )
