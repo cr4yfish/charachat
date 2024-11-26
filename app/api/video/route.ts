e30d1297edb04ae86c131cb68c09e19ce41ccc7e
Manuel <58395553+cr4yfish@users.noreply.github.com>
2024-11-26 19:42:19 +0100

    added image to video with FAL API key

--- app/api/video/route.ts
+++ app/api/video/route.ts
@@ -1,3 +1,6 @@
+import { getCurrentUser } from "@/functions/db/auth";
+import { getKeyServerSide } from "@/functions/serverHelpers";
+import { decryptMessage } from "@/lib/crypto";
 import { fal } from "@fal-ai/client";
 
 type RequestBody = {
@@ -7,6 +10,21 @@ type RequestBody = {
 
 export async function POST(req: Request) {
     const { imageLink, prompt } = await req.json() as RequestBody;
+
+    const profile = await getCurrentUser();
+
+    if(!profile.fal_gpt_encrypted_api_key) {
+        throw new Error("FAL API key not found. Image to Video is only available with a FAL API key.");
+    }
+
+    const key = await getKeyServerSide();
+    const keyBuffer = Buffer.from(key, "hex");
+
+    const decryptedKey = decryptMessage(profile.fal_gpt_encrypted_api_key, keyBuffer);
+
+    fal.config({
+        credentials: decryptedKey
+    })
 
     const result = await fal.subscribe("fal-ai/ltx-video/image-to-video", {
         input: {
