6357920dc12a61657f0b221f7ebb7d21c32b8ba4
Manuel <58395553+cr4yfish@users.noreply.github.com>
2024-12-01 21:39:31 +0100

    Used max length for schemas

--- lib/schemas.ts
+++ lib/schemas.ts
@@ -1,4 +1,5 @@
 import { z } from "zod";
+import { _PERSONA_MAX_LENGTH, _STORY_MAX_LENGHT } from "./maxLength";
 
 export const loginSchema = z.object({
     email: z.string().email({ message: "Invalid email address" }),
@@ -21,27 +22,23 @@ export const signUpSchema = z.object({
 export const storySchema = z.object({
     title: z.string()
         .min(5, { message: "Title must be at least 5 characters long" })
-        .max(100, { message: "Title must be at most 100 characters long" }),
+        .max(_STORY_MAX_LENGHT.title, { message: "Title must be at most 100 characters long" }),
     description: z.string()
-        .min(10, { message: "Description must be at least 10 characters long" })
-        .max(350, { message: "Description must be at most 350 characters long" }),
+        .max(_STORY_MAX_LENGHT.description, { message: "Description must be at most 350 characters long" }),
     story: z.string()
-        .min(100, { message: "Story must be at least 100 characters long" })
-        .max(10000, { message: "Story must be at most 2000 characters long" }),
+        .max(_STORY_MAX_LENGHT.story, { message: "Story must be at most 2000 characters long" }),
     first_message: z.string()
-        .min(20, { message: "First Message must be at least 20 characters long" })
-        .max(5000, { message: "First Message must be at most 500 characters long" }),
+        .max(_STORY_MAX_LENGHT.first_message, { message: "First Message must be at most 500 characters long" }),
     image_link: z.string()
-        .min(5, { message: "Image Link must be at least 5 characters long" })
         .url({ message: "Invalid URL" }),
 })
 
 export const personaSchema = z.object({
     fullName: z.string()
         .min(5, { message: "Name must be at least 5 characters long" })
-        .max(100, { message: "Name must be at most 50 characters long" }),
+        .max(_PERSONA_MAX_LENGTH.fullName, { message: "Name must be at most 50 characters long" }),
     bio: z.string()
-        .max(2000, { message: "Description must be at most 350 characters long" }),
+        .max(_PERSONA_MAX_LENGTH.bio, { message: "Description must be at most 350 characters long" }),
     avatarLink: z.string()
         .min(5, { message: "Image Link must be at least 5 characters long" })
         .url({ message: "Invalid URL" }),
