f4049eb9b3eea8ca2b48ca6c6d262c81d5bebf59
Manuel <58395553+cr4yfish@users.noreply.github.com>
2024-12-03 10:14:14 +0100

    Improved personacard small version

--- components/persona/PersonaCard.tsx
+++ components/persona/PersonaCard.tsx
@@ -8,6 +8,7 @@ import ConditionalLink from "../utils/ConditionalLink";
 import { safeParseLink, truncateText } from "@/lib/utils";
 import Icon from "../utils/Icon";
 import Markdown from "react-markdown";
+import { Avatar } from "@nextui-org/avatar";
 
 type Props = {
     data: Persona,
@@ -33,37 +34,37 @@ export default function PersonaCard(props: Props) {
                         h-[150px] w-[310px] dark:bg-zinc-800/40 backdrop-blur-xl border-none shadow-none
                         dark:hover:bg-zinc-700/40
                         ${props.fullWidth && "w-full"} 
-                        ${props.isSmall && "h-full"}
+                        ${props.isSmall && "h-[75px]"}
                         ${props.noBg && "dark:bg-transparent"} 
                     `}>
-                    <CardBody className="flex flex-row gap-4">
+                    <CardBody className={`flex flex-row gap-4 ${props.isSmall && "items-center"}`}>
                         
+                        {!props.isSmall &&
                         <div className="flex items-center justify-center">
                             <div className="relative h-[100%] w-[100px] overflow-hidden rounded-2xl">
                                 <Image className="relative object-cover" layout="fill" src={safeParseLink(props.data?.avatar_link)} alt={props.data.full_name} />
                             </div>
                         </div>
+                        }
 
-                        <div className="flex flex-col justify-between">
+                        {props.isSmall && <Avatar src={safeParseLink(props.data?.avatar_link)} className="min-w-[40px]" />}
+
+                        <div className={`flex flex-col justify-between ${props.isSmall && "w-full !flex-row pr-2"}`}>
                             <div className="flex flex-col gap-2">
                                 <div className="flex flex-col">
-                                    <div className="flex flex-col">
-                                        <h3 className="font-medium">{truncateText(props.data.full_name,40)}</h3>
-                                    </div>
+                                    <h3 className="font-medium">{truncateText(props.data.full_name,40)}</h3>
                                     <p className=" text-xs dark:text-zinc-400">By @{props.data.creator?.username}</p>
                                 </div>
-                                <Markdown className="text-xs max-w-md">{truncateText(props.data.bio ?? "", props.fullWidth ? 80 : 40)}</Markdown> 
+                                {!props.isSmall && <Markdown className="text-xs max-w-md">{truncateText(props.data.bio ?? "", props.fullWidth ? 80 : 40)}</Markdown> }
                             </div>
 
                          
-                            <div className="flex flex-row items-center gap-2">
-                                {props.data.is_private &&
-                                    <div className="flex gap-1 items-center">
-                                        <Icon color="green-500" downscale filled>lock</Icon>
-                                        <span className="text-xs text-green-500">Private</span>
-                                    </div>
-                                }
-                            </div>
+                            { props.data.is_private &&
+                                <div className="flex items-center gap-1">
+                                    <Icon color="green-500" downscale filled>lock</Icon>
+                                    <span className="text-xs text-green-500">Private</span>
+                                </div>
+                            }
                             
                         </div>
                     </CardBody>
