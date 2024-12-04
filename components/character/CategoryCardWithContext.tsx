098584e47d4d5a77afd42e77d58013e271820a11
Manuel <58395553+cr4yfish@users.noreply.github.com>
2024-12-04 19:00:27 +0100

    fixed categoryCardWithContext dark mode styling

--- components/character/CategoryCardWithContext.tsx
+++ components/character/CategoryCardWithContext.tsx
@@ -23,7 +23,7 @@ export default function CategoryCardWithContext(props: Props) {
             className={`
                 w-full max-w-fit flex items-center justify-center px-4 py-3 rounded-full bg-zinc-200/50 dark:bg-zinc-600/40
                 backdrop-blur-xl border-1 dark:border-none shadow-none text-sm font-medium
-                ${(props.data.id == currentCategory?.id) ? "bg-white dark:text-zinc-950" : "hover:bg-zinc-100 dark:hover:bg-zinc-700/40"}
+                ${(props.data.id == currentCategory?.id) ? "bg-white dark:bg-white dark:text-zinc-950" : "hover:bg-zinc-100 dark:hover:bg-zinc-700/40 dark:text-zinc-400"}
             `}
             >
             <CardBody className="p-0 w-full min-w-max h-full flex flex-row items-center justify-center">
