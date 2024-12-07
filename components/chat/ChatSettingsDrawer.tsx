7c2d7e84ddc2fc4478476781f4db96f5bd13576d
Manuel <58395553+cr4yfish@users.noreply.github.com>
2024-12-07 18:59:57 +0100

    Removed unused imports

--- components/chat/ChatSettingsDrawer.tsx
+++ components/chat/ChatSettingsDrawer.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState } from "react";
-import { Avatar } from "@nextui-org/avatar";
 import { Slider, SliderValue } from "@nextui-org/slider";
 import { Button } from "../utils/Button";
 import Icon from "../utils/Icon";
