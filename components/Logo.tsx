41cdca2f66acc12b2ee4b1893387bacf038c4923
Manuel <58395553+cr4yfish@users.noreply.github.com>
2024-12-07 18:56:05 +0100

    Added optional hideText for logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -5,13 +5,14 @@ type Props = {
     hideTextOnMobile?: boolean;
     hasLink?: boolean;
     className?: string;
+    hideText?: boolean;
 }
 
 export default function Logo(props: Props) {
     return (
         <ConditionalLink fullWidth href="/" active={props.hasLink ? true : false} className={`flex flex-row items-center gap-2 ${props.className}`}>
             <BotIcon size={24} />
-            <span className={`text-lg font-bold ${props.hideTextOnMobile && "max-sm:hidden"}`}>charachat</span>
+            {!props.hideText && <span className={`text-lg font-bold ${props.hideTextOnMobile && "max-sm:hidden"}`}>charachat</span>}
         </ConditionalLink>
         
     )
