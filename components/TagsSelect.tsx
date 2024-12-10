38fc66718913532ee7955b3b0b0cb03dbf0e7adc
Manuel <58395553+cr4yfish@users.noreply.github.com>
2024-12-10 11:07:57 +0100

    Fixed tagsSelect formatting

--- components/TagsSelect.tsx
+++ components/TagsSelect.tsx
@@ -86,7 +86,7 @@ export default function TagsSelect(props: Props) {
                     </DrawerClose>
                 </DrawerFooter>
             </DrawerContent>
-            </Drawer>
+        </Drawer>
 
         </>
     )
