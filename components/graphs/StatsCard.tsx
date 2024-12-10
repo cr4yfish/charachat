2cb62f8a4a9f31e16c8ee6b60ad49fd3d2d70e41
Manuel <58395553+cr4yfish@users.noreply.github.com>
2024-12-10 14:08:45 +0100

    Removed horizontal lines from statscard

--- components/graphs/StatsCard.tsx
+++ components/graphs/StatsCard.tsx
@@ -74,7 +74,7 @@ export function StatsCard(props: Props) {
               top: 20
             }}
           >
-            <CartesianGrid vertical={false} />
+            <CartesianGrid vertical={false} stroke="transparent" />
             <XAxis
               dataKey="timeframe"
               tickLine={false}
