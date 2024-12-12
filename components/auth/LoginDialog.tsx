c6f047f8a19e5dcd6b061acf1c140b4aac8af08a
Manuel <58395553+cr4yfish@users.noreply.github.com>
2024-12-12 22:18:23 +0100

    Fixed incorrect auth check

--- components/auth/LoginDialog.tsx
+++ components/auth/LoginDialog.tsx
@@ -9,7 +9,7 @@ import {
 } from "@/components/ui/dialog"
 
 import { useLoginDialog } from "@/context/LoginDialogProvider";
-import { getKeyClientSide } from "@/lib/crypto";
+import { getCurrentUser } from "@/functions/db/auth";
 import dynamic from "next/dynamic";
 import { useEffect } from "react";
 
@@ -19,12 +19,16 @@ export default function LoginDialog() {
     const { isOpen, setIsOpen, closeDialog, setIsLoggedIn, isLoggedIn } = useLoginDialog();
 
     useEffect(() => {
+
         if(!isLoggedIn) {
             try {
-                const key = getKeyClientSide();
-                if(key) {
-                    setIsLoggedIn(true);
-                }
+                getCurrentUser().then((user) => {
+                    if(user.user) {
+                        setIsLoggedIn(true)
+                    }
+                }).catch(() => {
+                    setIsLoggedIn(false);
+                })
             } catch {
                 setIsLoggedIn(false);
             }
