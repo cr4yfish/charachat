d7c4f9ffdfd508f2aebfd7c442f2d75d570a5b62
Manuel <58395553+cr4yfish@users.noreply.github.com>
2024-12-12 17:34:17 +0100

    Got rid of extra calling getUser

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -2,34 +2,19 @@
 
 import {
   Sidebar,
-  SidebarContent,
   SidebarFooter,
   SidebarGroup,
-  SidebarGroupLabel,
   SidebarHeader,
 } from "@/components/ui/sidebar"
 import Logo from "./Logo";
-import SidebarChatListFallback from "./sidebar/SidebarChatListFallback";
-import { getCurrentUser } from "@/functions/db/auth";
 import dynamic from "next/dynamic";
 import { Skeleton } from "./ui/skeleton";
-const SidebarChatListLoader = dynamic(() => import("./sidebar/SidebarChatListLoader"), { loading: () => <SidebarChatListFallback /> });
+
 const SidebarLink = dynamic(() => import ("./SidebarLink"), { loading: () => <Skeleton className="w-full h-full max-h-[48px]" />})
 const LoginButton = dynamic(() => import ("./auth/LoginButton"), { loading: () => <Skeleton className="w-full h-full max-h-[48px]" />})
 const ProfileCard = dynamic(() => import ("./user/ProfileCard"), { loading: () => <Skeleton className="w-full h-full max-h-[48px]" />})
 
 export async function LeftSidebar() {
-  let isLoggedIn = false;
-
-  try {
-    const user = await getCurrentUser();
-    if(user?.user) {
-      isLoggedIn = true;
-    }
-  } catch {
-    isLoggedIn = false;
-  }
-
   return (
     <Sidebar>
       <SidebarHeader className="p-2">
@@ -41,13 +26,6 @@ export async function LeftSidebar() {
           <SidebarLink link={`/chats`} icon="chat" label="Your Chats" />
         </SidebarGroup>
       </SidebarHeader>
-
-      {isLoggedIn &&
-      <SidebarContent className="p-2">
-        <SidebarGroupLabel className="text-lg font-bold">Chats</SidebarGroupLabel>
-        <SidebarChatListLoader />
-      </SidebarContent>
-      }
 
       <SidebarFooter className="w-full flex flex-col gap-2 items-start px-4 py-6">
         <LoginButton />    
