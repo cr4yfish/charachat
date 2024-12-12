2da38c728d572224ff815b0811d0ea5cab465c80
Manuel <58395553+cr4yfish@users.noreply.github.com>
2024-12-12 15:39:21 +0100

    Removed unused error handler

--- components/homepage/GeneralSwiper.tsx
+++ components/homepage/GeneralSwiper.tsx
@@ -19,18 +19,8 @@ export default async function GeneralSwiper(props: Props) {
         limit: 16,
     }
 
-    let initialData = [];
-    
-    try {
-        initialData = await props.loader(defaultLoad);
-    } catch (e) {
-        console.error(e);
-        return (
-            <>
-            <h1>Something went wrong loading popular Characters</h1>
-            </>
-        )
-    }
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const initialData = await props.loader(defaultLoad);
 
     return (
         <>
