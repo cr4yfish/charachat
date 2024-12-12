65879f2c85b20ce14f8d75f91bd22681d9b513e3
Manuel <58395553+cr4yfish@users.noreply.github.com>
2024-12-12 18:25:15 +0100

    Removed conditionalLink & animation from Character card

--- components/character/CharacterCard.tsx
+++ components/character/CharacterCard.tsx
@@ -1,14 +1,13 @@
 "use client";
 
-import { motion } from "motion/react";
 import { Card, CardBody } from "@nextui-org/card";
 import { Character } from "@/types/db";
-import ConditionalLink from "../utils/ConditionalLink";
 import { truncateText } from "@/lib/utils";
 import Icon from "../utils/Icon";
 import Markdown from "react-markdown";
 import Username from "../user/Username";
 import ImageWithBlur from "../ImageWithBlur";
+import Link from "next/link";
 
 type Props = {
     data: Character,
@@ -22,72 +21,66 @@ export default function CharacterCard(props: Props) {
 
     return (
         <>
-        <ConditionalLink active={props.hasLink} href={`/c/${props.data.id}`} fullWidth={props.fullWidth}>
-            <motion.div
-                initial={{ opacity: 0, y: 10 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.25 }}
-            >
-                <Card 
-                    isPressable={props.hasLink} 
-                    className={`
-                        h-[150px] w-[310px] bg-zinc-100/40 dark:bg-zinc-800/40 backdrop-blur-xl border dark:border-none shadow-none
-                        hover:bg-zinc-200/50 dark:hover:bg-zinc-700/40
-                        ${props.fullWidth && "w-full"} 
-                        ${props.isSmall && "h-full"}
-                        ${props.noBg && "dark:bg-transparent"} 
-                    `}>
-                    <CardBody className="flex flex-row gap-4">
+        <Link href={`/c/${props.data.id}`} className={`${props.fullWidth && "w-full"}`}>
+            <Card 
+                isPressable={props.hasLink} 
+                className={`
+                    h-[150px] w-[310px] bg-zinc-100/40 dark:bg-zinc-800/40 backdrop-blur-xl border dark:border-none shadow-none
+                    hover:bg-zinc-200/50 dark:hover:bg-zinc-700/40
+                    ${props.fullWidth && "w-full"} 
+                    ${props.isSmall && "h-full"}
+                    ${props.noBg && "dark:bg-transparent"} 
+                `}>
+                <CardBody className="flex flex-row gap-4">
+                    
+                    <div className="flex items-center justify-center">
+                        <div className="relative h-[100%] w-[100px] overflow-hidden rounded-2xl">
+                            <ImageWithBlur 
+                                src={props.data.image_link}
+                                alt={props.data.name ?? "avatar"}
+                                fill
+                                sizes="100px"
+                                className="relative object-cover"
+                                is_nsfw={props.data.is_nsfw}
+                            />
+                        </div>
+                    </div>
+
+                    <div className="flex flex-col justify-between">
+                        <div className="flex flex-col gap-2">
+                            <div className="flex flex-col">
+                                <h3 className="font-medium">{truncateText(props.data.name,40)}</h3>
+                                <Username profile={props.data.owner} />
+                            </div>
+                            <Markdown className="text-xs max-w-md">{truncateText(props.data.description, props.fullWidth ? 80 : 40)}</Markdown> 
+                        </div>
+
                         
-                        <div className="flex items-center justify-center">
-                            <div className="relative h-[100%] w-[100px] overflow-hidden rounded-2xl">
-                                <ImageWithBlur 
-                                    src={props.data.image_link}
-                                    alt={props.data.name ?? "avatar"}
-                                    fill
-                                    sizes="100px"
-                                    className="relative object-cover"
-                                    is_nsfw={props.data.is_nsfw}
-                                />
-                            </div>
-                        </div>
-
-                        <div className="flex flex-col justify-between">
-                            <div className="flex flex-col gap-2">
-                                <div className="flex flex-col">
-                                    <h3 className="font-medium">{truncateText(props.data.name,40)}</h3>
-                                    <Username profile={props.data.owner} />
+                        <div className="flex flex-row items-center gap-2">
+                            {((props.data.chats !== undefined) && (props.data.likes !== undefined)) &&
+                            <>
+                                <div className="flex items-center gap-1 text-xs dark:text-zinc-400">
+                                    <Icon downscale>chat_bubble</Icon>
+                                    <span>{props.data.chats}</span>
                                 </div>
-                                <Markdown className="text-xs max-w-md">{truncateText(props.data.description, props.fullWidth ? 80 : 40)}</Markdown> 
-                            </div>
-
-                         
-                            <div className="flex flex-row items-center gap-2">
-                                {((props.data.chats !== undefined) && (props.data.likes !== undefined)) &&
-                                <>
-                                    <div className="flex items-center gap-1 text-xs dark:text-zinc-400">
-                                        <Icon downscale>chat_bubble</Icon>
-                                        <span>{props.data.chats}</span>
-                                    </div>
-                                    <div className="flex items-center gap-1 text-xs dark:text-zinc-400">
-                                        <Icon downscale>favorite</Icon>
-                                        <span>{props.data.likes}</span>
-                                    </div>
-                                </>
-                                } 
-                                {props.data.is_private &&
-                                    <div className="flex gap-1 items-center">
-                                        <Icon color="green-500" downscale filled>lock</Icon>
-                                        <span className="text-xs text-green-500">Private</span>
-                                    </div>
-                                }
-                            </div>
-                            
+                                <div className="flex items-center gap-1 text-xs dark:text-zinc-400">
+                                    <Icon downscale>favorite</Icon>
+                                    <span>{props.data.likes}</span>
+                                </div>
+                            </>
+                            } 
+                            {props.data.is_private &&
+                                <div className="flex gap-1 items-center">
+                                    <Icon color="green-500" downscale filled>lock</Icon>
+                                    <span className="text-xs text-green-500">Private</span>
+                                </div>
+                            }
                         </div>
-                    </CardBody>
-                </Card>
-            </motion.div>
-        </ConditionalLink>
+                        
+                    </div>
+                </CardBody>
+            </Card>
+        </Link>
         </>
     )
 }
