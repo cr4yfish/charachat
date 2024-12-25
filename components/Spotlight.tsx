40788c27a5667e92b5c6f49bb181c12ffc52b252
Manuel <58395553+cr4yfish@users.noreply.github.com>
2024-12-25 09:45:20 +0100

    Fixed unsupported image types in color extractor

--- components/Spotlight.tsx
+++ components/Spotlight.tsx
@@ -8,27 +8,29 @@ import Vibrant from 'node-vibrant';
 
 export default async function Spotlight() {
     const character = await getNewestCharacter();
-
     const imageUrl = safeParseLink(character.image_link);
-    const palette = await Vibrant.from(imageUrl).getPalette();
 
-    return (
-        <div className="w-full h-[320px] relative overflow-visible">
-            <Card className={`h-full relative overflow-hidden rounded-3xl bg-transparent z-10 dark:bg-neutral-900/50`}>
-                <div className="absolute top-0 left-0 w-full h-full dark:opacity-10 opacity-90 " style={{ backgroundColor: palette.DarkVibrant?.hex }}></div>
-                
-                <CardHeader className="z-10 relative">
-                    <CardDescription className={`font-medium text-md dark:text-neutral-200 text-neutral-200`}>Check out this Character</CardDescription>
-                    <CardTitle className="text-4xl font-bold" style={{ color: palette.LightVibrant?.hex }}>Spotlight</CardTitle>
-                </CardHeader>
-                <CardContent>
-                    <CharacterCard data={character} hasLink fullWidth bgOverwrite="bg-white" />
-                </CardContent>
-            </Card>
-            <div 
-                className={`absolute select-none pointer-events-none scale-[150%] scale-y-[300%] -translate-y-[200px] opacity-15 dark:opacity-20 blur-3xl top-0 left-0 w-full h-full`}
-                style={{  backgroundColor: palette.Vibrant?.hex }}
-            ></div>
-        </div>
+    const isSupportedUrl = !imageUrl.includes(".webp");
+
+    const palette = isSupportedUrl && await Vibrant.from(imageUrl).getPalette();
+
+return (
+    <div className="w-full h-[320px] relative overflow-visible">
+        <Card className={`h-full relative overflow-hidden rounded-3xl bg-transparent z-10 dark:bg-neutral-900/50`}>
+            <div className="absolute top-0 left-0 w-full h-full dark:opacity-10 opacity-90 " style={{ backgroundColor: palette ? palette.DarkVibrant?.hex : "" }}></div>
+            
+            <CardHeader className="z-10 relative">
+                <CardDescription className={`font-medium text-md dark:text-neutral-200 text-neutral-200`}>Check out this Character</CardDescription>
+                <CardTitle className="text-4xl font-bold" style={{ color: palette ? palette.LightVibrant?.hex : "" }}>Spotlight</CardTitle>
+            </CardHeader>
+            <CardContent>
+                <CharacterCard data={character} hasLink fullWidth bgOverwrite="bg-white" />
+            </CardContent>
+        </Card>
+        <div 
+            className={`absolute select-none pointer-events-none scale-[150%] scale-y-[300%] -translate-y-[200px] opacity-15 dark:opacity-20 blur-3xl top-0 left-0 w-full h-full`}
+            style={{  backgroundColor: palette ? palette.Vibrant?.hex : "" }}
+        ></div>
+    </div>
     )
 }
