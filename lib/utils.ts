1b9a8d4dcac842658d2bc2b47fbec446bcf8ad4f
Manuel <58395553+cr4yfish@users.noreply.github.com>
2025-01-14 13:41:06 +0100

    increased code safety

--- lib/utils.ts
+++ lib/utils.ts
@@ -126,7 +126,7 @@ export async function sleep(ms: number) {
 }
 
 export function replaceVariables(text = "", variables: Record<string, string>) {
-  return text.replace(/\${(.*?)}/g, (_, match) => {
+  return text?.replace(/\${(.*?)}/g, (_, match) => {
     return variables[match] || '';
   });
 }
