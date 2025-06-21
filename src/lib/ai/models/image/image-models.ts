d6eba703cbac865e90a9b1cf553e6849d055652a
Manuel <58395553+cr4yfish@users.noreply.github.com>
2025-06-21 20:15:59 +0200

    Fixed typo

--- src/lib/ai/models/image/image-models.ts
+++ src/lib/ai/models/image/image-models.ts
@@ -5,35 +5,35 @@ export const imageModels: ImageModel[] = [
         id: "black-forest-labs/flux-schnell",
         title: "Flux Schnell",
         style: "Flux Fast",
-        provider: "replicate",
+        provider: "Replicate",
         type: "text-to-image"
     },
     {
         id: "black-forest-labs/flux-1.1-pro",
         title: "Flux Pro",
         style: "Flux Pro",
-        provider: "replicate",
+        provider: "Replicate",
         type: "text-to-image"
     },
     {
         id: "black-forest-labs/flux-1.1-pro-ultra",
         title: "Flux Pro Ultra",
         style: "Flux Ultra",
-        provider: "replicate",
+        provider: "Replicate",
         type: "text-to-image"
     },
     {
         id: "google/imagen-3",
         title: "Imagen 3",
         style: "Imagen 3",
-        provider: "replicate",
+        provider: "Replicate",
         type: "text-to-image"
     },
     {
         id: "cjwbw/animagine-xl-3.1:6afe2e6b27dad2d6f480b59195c221884b6acc589ff4d05ff0e5fc058690fbb9",
         title: "animagine xl 3.1",
         style: "Animagine3",
-        provider: "replicate",
+        provider: "Replicate",
         type: "text-to-image"
     },
 ]
