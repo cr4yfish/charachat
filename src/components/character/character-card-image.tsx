cb6945bfb4c57e2f5af8d18d09a792f8f19db80a
Manuel <58395553+cr4yfish@users.noreply.github.com>
2025-06-21 15:20:15 +0200

    Added chat stats & fixed border radius to character image cards

--- src/components/character/character-card-image.tsx
+++ src/components/character/character-card-image.tsx
@@ -6,6 +6,7 @@ import ImageWithBlur from "../image/imageWithBlur";
 import Link from "next/link";
 import { Card, CardContent } from "../ui/card";
 import { memo } from "react";
+import { MessageCircleIcon } from "lucide-react";
 
 type CardProps = {
     data: Character;
@@ -19,11 +20,18 @@ const PureImageCard = (props: CardProps) => {
                    
                     <CardContent className="absolute z-10 bottom-0 left-0 w-full p-0 overflow-visible rounded-b-2xl">
                         <div className="flex flex-col pb-2 px-3 pt-1 z-10 relative">
-                            <h3 className="font-medium text-sm truncate ">{truncateText(props.data.name, 15)}</h3>
-                            {props.data.description && <p className="text-xs truncate text-muted-foreground opacity-75 w-[80%] " >{props.data.description}</p>}
+                            <h3 className="font-medium text-sm truncate text-neutral-200 ">{truncateText(props.data.name, 15)}</h3>
+                            <div className="flex flex-row justify-between items-center text-xs text-muted-foreground opacity-75">
+                                {props.data.description && <p className=" truncate w-[80%] " >{props.data.description}</p>}
+                                <span className="flex items-center gap-1 ml-0.5 ">
+                                    <MessageCircleIcon size={12} /> 
+                                    {props.data.chats?.toLocaleString()}
+                                </span>
+                            </div>
+                           
                         </div>
                         {/* Background gradient */}
-                        <div className="absolute bottom-0 left-0 size-full h-[150%] bg-gradient-to-t from-neutral-900/75 via-neutral-900/33 to-neutral-900/0"></div>
+                        <div className="absolute rounded-2xl bottom-0 left-0 size-full h-[150%] bg-gradient-to-t from-neutral-900/75 via-neutral-900/33 to-neutral-900/0"></div>
                     </CardContent>
 
                     <div className="absolute top-0 left-0 w-full h-full overflow-hidden rounded-2xl">
