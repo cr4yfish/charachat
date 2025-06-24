f77cbfcc32731f1e0fe9f8cfadd234d29a6e0eeb
Manuel <58395553+cr4yfish@users.noreply.github.com>
2025-06-24 14:25:53 +0200

    Changed welcomeCard to only show when signed in

--- src/components/explore/welcome-card.tsx
+++ src/components/explore/welcome-card.tsx
@@ -3,9 +3,14 @@
 import { ArrowRightIcon } from "lucide-react";
 import { CarouselItem } from "../ui/carousel";
 import Link from "next/link";
+import { useAuth } from "@clerk/nextjs";
 
 
 function WelcomeCard() {
+    const { isSignedIn } = useAuth();
+
+    if(!isSignedIn) return null;
+
     return (
         <CarouselItem className="min-lg:basis-1/3">
             <Link href={"/home/settings/migrate"} className="w-full">
