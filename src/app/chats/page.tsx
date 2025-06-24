8b081bb5273e07f10d9ffa29edd2cb41759850c7
Manuel <58395553+cr4yfish@users.noreply.github.com>
2025-06-24 14:29:24 +0200

    Added sign in button to chats page

--- src/app/chats/page.tsx
+++ src/app/chats/page.tsx
@@ -1,7 +1,19 @@
 import Chats from "@/components/chats/chats";
+import { SignInButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
 
 
-export default function ChatsPage() {
+export default async function ChatsPage() {
+
+    const user = await currentUser();
+
+    if(!user) {
+        return (
+            <div className="flex flex-col items-center justify-center h-screen">
+                <SignInButton />
+            </div>
+        );
+    }
 
     return (
         <div className="flex flex-col items-center justify-center h-screen w-full">
