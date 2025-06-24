7f2a2fb245a74de0e7bdf1b70f382439cfd9c90f
Manuel <58395553+cr4yfish@users.noreply.github.com>
2025-06-24 13:40:48 +0200

    Fixed migration link

--- src/components/explore/welcome-card.tsx
+++ src/components/explore/welcome-card.tsx
@@ -8,7 +8,7 @@ import Link from "next/link";
 function WelcomeCard() {
     return (
         <CarouselItem className="min-lg:basis-1/3">
-            <Link href={"/migrate"} className="w-full">
+            <Link href={"/home/settings/migrate"} className="w-full">
                 <div className="flex flex-row justify-between items-center gap-2 rounded-3xl text-fuchsia-200/80 bg-fuchsia-800 p-4 cursor-pointer hover:bg-fuchsia-700 transition-all duration-200">
                     <div className="flex flex-col gap-1">
                         <p className="text-xs">Did you use Charachat v1?</p>
