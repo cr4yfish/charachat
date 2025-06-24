a28bfbfc04de3ddfa4323dc6f722626dc7fb309d
Manuel <58395553+cr4yfish@users.noreply.github.com>
2025-06-24 13:35:51 +0200

    Added missing random cahracter route

---
+++ src/app/api/characters/random/route.ts
@@ -0,0 +1,17 @@
+import { getRandomCharacters } from "@/lib/db/character";
+import { TIMINGS } from "@/lib/timings";
+import { unstable_cache } from 'next/cache';
+
+export async function GET() {
+
+    const res = await unstable_cache(
+        async () => await getRandomCharacters(),
+        [`random-characters`],
+        {
+            revalidate: TIMINGS.ONE_MINUTE,
+            tags: ['characters'],
+        }
+    )();
+
+    return Response.json(res);
+}
