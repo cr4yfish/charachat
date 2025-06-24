4f429c09f74e56fe306694b35747946654c3b708
Manuel <58395553+cr4yfish@users.noreply.github.com>
2025-06-24 15:01:42 +0200

    Added a little bottom margin to drawer footer to account for phone UI

--- src/components/ui/drawer.tsx
+++ src/components/ui/drawer.tsx
@@ -90,7 +90,7 @@ function DrawerFooter({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
       data-slot="drawer-footer"
-      className={cn("mt-auto flex flex-col gap-2 p-4", className)}
+      className={cn("mt-auto flex flex-col gap-2 p-4 mb-4", className)}
       {...props}
     />
   )
