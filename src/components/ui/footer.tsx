1acbb2fc8b09ebbeb01975fe51e5f5d4ecf32d15
Manuel <58395553+cr4yfish@users.noreply.github.com>
2025-06-25 20:24:34 +0200

    Fixed footer

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -7,29 +7,29 @@ export default function Footer() {
     const currentYear = new Date().getFullYear();
 
     return (
-        <footer className="w-full flex max-md:flex-col gap-6 pt-4 text-xs text-muted-foreground">
+        <footer className="w-full flex flex-col gap-6 pt-4">
 
             <Separator />
+            <div className="w-full flex items-start max-md:flex-col gap-6 text-xs text-muted-foreground">
+                <span className="flex items-center gap-1 font-bold text-white/90"><BotIcon />Charachat </span>
 
-            <span className="flex items-center gap-1 font-bold text-white/90"><BotIcon />Charachat </span>
+                <div className="flex flex-col gap-2">
+                    <span className="font-medium text-white/90 ">About Charachat</span>
+                    <Link className="hover:underline" href={"https://www.reddit.com/r/Charachat"}  target="_blank">Reddit</Link>
+                    <Link className="hover:underline" href={"https://discord.gg/2HqqwcwGCy"}  target="_blank">Discord</Link>
+                    <Link className="hover:underline" href={"https://github.com/cr4yfish/charachat"}  target="_blank">GitHub</Link>
+                </div>
 
-            <div className="flex flex-col gap-2">
-                <span className="font-medium text-white/90 ">About Charachat</span>
-                <Link className="hover:underline" href={"https://www.reddit.com/r/Charachat"}  target="_blank">Reddit</Link>
-                <Link className="hover:underline" href={"https://discord.gg/2HqqwcwGCy"}  target="_blank">Discord</Link>
-                <Link className="hover:underline" href={"https://github.com/cr4yfish/charachat"}  target="_blank">GitHub</Link>
+                <div className="flex flex-col gap-2">
+                    <span className="font-medium text-white/90">Resources</span>
+                    <Link className="hover:underline" href={"https://github.com/cr4yfish/charachat/blob/v2/README.md"} target="_blank">About Charachat</Link>
+                    <Link className="hover:underline" href={"/pricing"}>Pricing</Link>
+                    <Link className="hover:underline" href={"/"}>Explore</Link>
+                    <Link className="hover:underline" href={"/search"}>Search</Link>
+                </div>
+
+                <span>© {currentYear} Charachat</span>
             </div>
-
-            <div className="flex flex-col gap-2">
-                <span className="font-medium text-white/90">Resources</span>
-                <Link className="hover:underline" href={"https://github.com/cr4yfish/charachat/blob/v2/README.md"} target="_blank">About Charachat</Link>
-                <Link className="hover:underline" href={"/pricing"}>Pricing</Link>
-                <Link className="hover:underline" href={"/"}>Explore</Link>
-                <Link className="hover:underline" href={"/search"}>Search</Link>
-            </div>
-
-            <span>© {currentYear} Charachat</span>
-
         </footer>
     );
 }
