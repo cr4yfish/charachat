7276dba94a1d1ff295c0465c66a09f1ec7996788
Manuel <58395553+cr4yfish@users.noreply.github.com>
2025-06-25 16:30:24 +0200

    Improved total character stats card

--- src/components/stats/total-character-stats.tsx
+++ src/components/stats/total-character-stats.tsx
@@ -4,11 +4,13 @@ import { API_ROUTES } from "@/lib/constants/apiRoutes";
 import { TIMINGS_MILLISECONDS } from "@/lib/constants/timings";
 import { fetcher } from "@/lib/utils";
 import useSWR from "swr";
-import { Card, CardContent, CardDescription, CardHeader } from "../ui/card";
 import { Stats } from "@/lib/db/types";
+import { ArrowRightIcon } from "lucide-react";
+import Link from "next/link";
+import Spinner from "../ui/spinner";
 
 export default function TotalCharacterStats() {
-    const { data } = useSWR<Stats>(API_ROUTES.GET_TOTAL_CHARACTER_STATS, fetcher, {
+    const { data, isLoading } = useSWR<Stats>(API_ROUTES.GET_TOTAL_CHARACTER_STATS, fetcher, {
         revalidateIfStale: false,
         revalidateOnFocus: false,
         revalidateOnReconnect: false,
@@ -16,17 +18,17 @@ export default function TotalCharacterStats() {
     })
     
     return (
-        <div className="flex flex-col gap-2">
-            <Card className="!w-[240px] !h-[80px] flex flex-col gap-0 py-3  rounded-3xl ">
-                <CardHeader className="pb-0">
-                    <CardDescription>Total number of Characters</CardDescription>
-                </CardHeader>
-                <CardContent className="pt-0 pb-0">
-                    <div className="flex items-center justify-between gap-2">
-                        <span className="text-xl font-bold capitalize">{data?.count?.toLocaleString()}</span>
-                    </div>
-                </CardContent>
-            </Card>
-        </div>
+        <Link href={"/search"} className="h-[80px] w-[260px] shrink-0 flex flex-row justify-between items-center gap-2 rounded-3xl text-rose-900 bg-rose-400 p-4 cursor-pointer hover:bg-rose-500 transition-all duration-200">
+            <div className="flex flex-col gap-1">
+                <p className="text-xs">Wanna see more?</p>
+                <div className="flex items-center gap-1">
+                    <h2 className="font-bold flex items-center gap-2">Browse {isLoading ? <Spinner/> : data?.count} Chars</h2>
+                </div>
+                
+            </div>
+            <div className="flex items-center self-end ">
+                <ArrowRightIcon color="currentColor" className="self-end" />
+            </div>
+        </Link>
     );
 }
