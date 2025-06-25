1307198df4c7c5af9a01874596ff6e00f784ce14
Manuel <58395553+cr4yfish@users.noreply.github.com>
2025-06-25 12:03:37 +0200

    Added char fields length limits

---
+++ src/lib/constants/lengthLimits.ts
@@ -0,0 +1,14 @@
+
+
+export const CHARACTER_LENGTH_LIMITS = {
+    name: 100,
+    description: 5000,
+    bio: 10000,
+    intro: 10000,
+    first_message: 10000,
+    scenario: 10000,
+    personality: 10000,
+    book: 10000,
+    system_prompt: 5000,
+    image_prompt: 5000,
+}
