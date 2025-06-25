31dcf90aeba01d32016d75915c967e69d257aebd
Manuel <58395553+cr4yfish@users.noreply.github.com>
2025-06-25 12:03:49 +0200

    Improved how appTabbar handles disabled paths

--- src/components/ui/tab-bar/app-tabbar.tsx
+++ src/components/ui/tab-bar/app-tabbar.tsx
@@ -6,6 +6,7 @@ import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
 import { motion } from "motion/react";
 import { useSidebar } from "../sidebar";
+import { useMemo } from "react";
 
 export type NavItem = {
     name: string;
@@ -19,16 +20,31 @@ export const tabs: NavItem[] = [
     { name: "Home", href: "/home", icon: <HomeIcon /> }
 ]
 
+const disabledPaths = ["/chat", "/search", "/chats", "/home", "/explore", "/edit"];
+
+function checkIsPathDisabled(pathname: string, disabledPaths: string[]): boolean {
+    return disabledPaths.some(path => {
+        if (path.endsWith('/')) {
+            return pathname.startsWith(path);
+        }
+        return pathname.includes(path);
+    });
+}
+
 export function AppTabBar() {
     const pathname = usePathname();
     const { isMobile } = useSidebar();
+
+    const isPathDisabled = useMemo(() => {
+        return checkIsPathDisabled(pathname, disabledPaths);
+    }, [pathname]);
 
     if(!isMobile) return null; // Hide tab bar on desktop
 
     return (
         <motion.div 
             initial={{ bottom: "-90px" }}
-            animate={ (pathname === "/chat" || pathname.includes("/chat/") || pathname === "/search") ? { bottom: "-120px"} : { bottom: "0px" }}
+            animate={ isPathDisabled ? { bottom: "-120px"} : { bottom: "0px" }}
             exit={{ bottom: "-90px" }}
             className={cn("transition all flex items-center justify-center gap-2 w-full h-[60px] fixed bottom-0 left-0 z-50")}
         >
