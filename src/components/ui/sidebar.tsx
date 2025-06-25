b11a69e8a05e48fd9ddcd82e277862661eacb36b
Manuel <58395553+cr4yfish@users.noreply.github.com>
2025-06-25 20:24:49 +0200

    Fixed sidebar overflow stuff

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -310,7 +310,7 @@ function SidebarInset({ className, ...props }: React.ComponentProps<"main">) {
     <main
       data-slot="sidebar-inset"
       className={cn(
-        "bg-background relative flex w-full h-full max-h-screen overflow-hidden overflow-y-auto flex-1 flex-col max-md:basis-1/2 !items-center justify-center [&>*:first-child]:max-w-[1920px]",
+        "bg-background relative flex w-full h-full max-h-screen overflow-hidden overflow-y-auto flex-1 flex-col max-md:basis-1/2 [&>*:first-child]:max-w-[1920px]",
         "md:peer-data-[variant=inset]:m-2 md:peer-data-[variant=inset]:ml-0 md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow-sm md:peer-data-[variant=inset]:peer-data-[state=collapsed]:ml-2",
         className
       )}
