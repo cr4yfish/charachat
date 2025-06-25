8f7e2d2530a662e171aef60ac8b71793490975ab
Manuel <58395553+cr4yfish@users.noreply.github.com>
2025-06-25 14:09:10 +0200

    Bumped loading limits

--- src/lib/constants/limits.ts
+++ src/lib/constants/limits.ts
@@ -2,13 +2,27 @@
 
 export const LIMITS = {
 
-    MAX_PERSONAS_PER_PAGE: 20,
-    MAX_CHATS_PER_PAGE: 20,
+    /**
+     * These are used for infinite scroll pagination.
+     */
+    MAX_PERSONAS_PER_PAGE: 31,
+    MAX_CHATS_PER_PAGE: 31,
     MAX_CHARACTERS_PER_PAGE: 31,
+    MAX_TRENDING_CHARACTERS_PER_PAGE: 31,
+
+    /**
+     * 25 is probably more than the total number of categories in the database.
+     */
+    MAX_CATEGORIES_PER_PAGE: 25,
+
+    // Used e.g. in /search
     MAX_CHARACTERS_PAGINATION: 50,
-    MAX_CATEGORIES_PER_PAGE: 15,
-    MAX_TRENDING_CHARACTERS_PER_PAGE: 21,
+    
+    
+    
     MAX_MESSAGES_PER_PAGE: 100,
     MAX_MESSAGES_IN_CHAT: 200,
+
+
     MAX_RAG_RESULTS: 5,
 }
