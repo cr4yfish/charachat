a542f306192612846ea7bd0078739e1f607f8352
Manuel <58395553+cr4yfish@users.noreply.github.com>
2025-06-26 19:55:22 +0200

    Increased footer gaps

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -13,14 +13,14 @@ export default function Footer() {
             <div className="w-full flex items-start max-md:flex-col gap-6 text-xs text-muted-foreground">
                 <span className="flex items-center gap-1 font-bold text-white/90"><BotIcon />Charachat </span>
 
-                <div className="flex flex-col gap-2">
+                <div className="flex flex-col gap-3 sm:gap-2">
                     <span className="font-medium text-white/90 ">About Charachat</span>
                     <Link className="hover:underline" href={"https://www.reddit.com/r/Charachat"}  target="_blank">Reddit</Link>
                     <Link className="hover:underline" href={"https://discord.gg/2HqqwcwGCy"}  target="_blank">Discord</Link>
                     <Link className="hover:underline" href={"https://github.com/cr4yfish/charachat"}  target="_blank">GitHub</Link>
                 </div>
 
-                <div className="flex flex-col gap-2">
+                <div className="flex flex-col gap-3 sm:gap-2">
                     <span className="font-medium text-white/90">Resources</span>
                     <Link className="hover:underline" href={"https://github.com/cr4yfish/charachat/blob/v2/README.md"} target="_blank">About Charachat</Link>
                     <Link className="hover:underline" href={"/pricing"}>Pricing</Link>
