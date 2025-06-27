7acec906b13b69b2a78045bcf16361fb6de1f344
Manuel <58395553+cr4yfish@users.noreply.github.com>
2025-06-27 22:16:54 +0200

    Added conditional link, onclick & skeleton to personaSmallCard

--- src/components/personas/persona-small-card.tsx
+++ src/components/personas/persona-small-card.tsx
@@ -3,32 +3,33 @@
 import { motion } from "motion/react"
 import { cn } from "@/lib/utils";
 import ImageWithBlur from "../image/imageWithBlur";
-import Link from "next/link";
 import { Card, CardContent } from "../ui/card";
 import { LockIcon } from "lucide-react";
-import { memo, useState } from "react";
+import { memo } from "react";
 import equal from "fast-deep-equal";
 import { Persona } from "@/lib/db/types/persona";
-import { safeParseLink } from "@/lib/utils/text";
+import { safeParseLink, truncateText } from "@/lib/utils/text";
+import ConditionalLink from "../conditional-link";
+import { Skeleton } from "../ui/skeleton";
 
 type Props = {
     data: Persona,
     hasLink: boolean,
+    onClick?: () => void,
 }
 
 function PurePersonaSmallCard(props: Props) {
-    const [isHovered, setIsHovered] = useState(false);
 
     return (
         <>
-        <Link href={`/p/${props.data.id}`} className="w-full max-w-[560px]" onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
+        <ConditionalLink href={`/p/${props.data.id}`} className="w-full max-w-[560px]" active={props.hasLink} >
             <motion.div
                 initial={{ opacity: 0, y: 10 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.25 }}
+                onClick={props.onClick}
             >
-                <Card className={cn("h-[72px] sm:h-[90px]  w-full overflow-hidden p-3 dark:bg-slate-800/5 border shadow-none dark:hover:bg-slate-800/30 flex flex-row items-center justify-start gap-3 transition-all", {
-                    "dark:bg-slate-800/40": isHovered,
+                <Card className={cn("h-[72px] sm:h-[90px] w-full overflow-hidden p-3 dark:bg-slate-800/5 border sm:border-none shadow-none dark:hover:bg-slate-800/30 flex flex-row items-center justify-start gap-3 transition-all", {
                     "dark:border-emerald-400": props.data.is_private,
                 })}  >
 
@@ -44,17 +45,19 @@ function PurePersonaSmallCard(props: Props) {
                         />
                     </div>
 
-
                     <CardContent className="flex flex-col flex-1 gap-1 p-0 w-full overflow-hidden ">
 
-                        <div className="flex flex-col">
-                            <div className="flex flex-col">
+                        <div className="flex flex-col items-start">
+
+                            <div className="flex flex-col items-start w-full text-start">
                                 <h3 className="font-medium m-0 text-sm sm:text-lg truncate ">{props.data.full_name}</h3>
-                                {/* <span className="text-xs text-neutral-600 dark:text-neutral-400">@{props.data.owner?.username}</span> */}
+                                {(props.data.description || props.data.bio) && 
+                                <p className="text-xs sm:text-sm text-muted-foreground truncate w-[75%] overflow-hidden">
+                                    {truncateText(props.data.description || props.data.bio, 30) }
+                                </p>
+                                }                                
                             </div>
-                            <p className="text-xs sm:text-sm text-muted-foreground truncate w-[75%]">
-                                {props.data.description}
-                            </p>
+                            <span className="text-xs sm:text-sm text-muted-foreground text-start">@{props.data.creator?.username}</span>
                         </div>
 
                         
@@ -73,7 +76,7 @@ function PurePersonaSmallCard(props: Props) {
                     </CardContent>
                 </Card>
             </motion.div>
-        </Link>
+        </ConditionalLink>
         </>
     )
 }
@@ -85,3 +88,21 @@ const PersonaSmallCard =  memo(PurePersonaSmallCard, (prevProps, nextProps) => {
 });
 
 export default PersonaSmallCard;
+
+export const PersonaSmallCardSkeleton = () => {
+
+    return (
+        <Card className="h-[72px] sm:h-[90px] min-w-[245px]  w-full overflow-hidden p-3 dark:bg-slate-800/5 border sm:border-none shadow-none flex flex-row items-center justify-start gap-3">
+            {/* Avatar skeleton */}
+            <Skeleton className="size-[52px] sm:size-[90px] rounded-lg shrink-0 bg-primary/15" />
+            
+            {/* Content skeleton */}
+            <div className="flex flex-col gap-1 w-[155px] overflow-hidden">
+                {/* Name skeleton */}
+                <Skeleton className="h-4 sm:h-[28px] w-[120px] bg-primary/15" />
+                {/* Description skeleton */}
+                <Skeleton className="h-3 sm:h-4 w-1/2 bg-primary/15" />
+            </div>
+        </Card>
+    )
+}
