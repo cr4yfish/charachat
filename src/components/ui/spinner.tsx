39587f6a269bf1758c100c98c7a3bebe2904b27d
Manuel <58395553+cr4yfish@users.noreply.github.com>
2025-06-27 22:15:58 +0200

    Improved spinner

--- src/components/ui/spinner.tsx
+++ src/components/ui/spinner.tsx
@@ -5,12 +5,21 @@ type Props = {
 }
 
 export default function Spinner(props: Props) {
+    // return (
+    //     <div className={cn("size-3 animate-spin rounded-full border-2 border-solid border-current border-t-transparent", {
+    //         "size-2": props.size === "small",
+    //         "size-3": props.size === "medium",
+    //         "size-4": props.size === "large"
+    //     })}
+    //     ></div>
+    // )
     return (
-        <div className={cn("size-3 animate-spin rounded-full border-2 border-solid border-current border-t-transparent", {
+        <div className={cn("animate-spin rounded-full size-2 border-b-2 border-primary", {
             "size-2": props.size === "small",
             "size-3": props.size === "medium",
             "size-4": props.size === "large"
-        })}
-        ></div>
+        })}>
+
+        </div>
     )
 }
